eb3af9bdb2daac4778bc6152e00c5c30222c8d01
hilyafadhilah <hilyafadhilah@gmail.com>
2020-11-14 14:47:25 +0700

    First draft

---
+++ main.py
@@ -0,0 +1,62 @@
+# -----------------------------------------------------------
+# main.py
+#
+# Tugas Besar Mata Kuliah Pengenalan Komputasi
+#
+# (C) 2020 Hilya F. Imania, Gibran Darmawan,
+# Joy P. Sitanggang, and Rd. Elviana L.
+# Jakarta, Indonesia
+# Released under MIT License (MIT)
+# -----------------------------------------------------------
+
+import unit
+
+############################
+
+def apaini(modes):
+    unit.print_mode(modes)
+
+    mode = unit.input_mode(modes)
+    durasi = unit.input_durasi()
+
+    unit.switch_lampu(True)
+    unit.switch_magnetron(True)
+    unit.switch_kipas(True)
+    unit.heat(mode, durasi)
+    unit.switch_lampu(False)
+    unit.switch_magnetron(False)
+    unit.switch_kipas(False)
+    unit.beep()
+    unit.buka_pintu()
+
+def main():
+    modes = ['micro', 'grill', 'defrost', 'popcorn']
+
+    while True:
+        unit.buka_pintu()
+        unit.switch_lampu(True)
+
+        food = unit.input_makanan()
+
+        unit.switch_makanan(True, food)
+        unit.tutup_pintu()
+        unit.switch_lampu(False)
+
+        while True:
+            apaini(modes)
+
+            if unit.keluarkan_makanan(food):
+                unit.switch_makanan(False, food)
+                unit.tutup_pintu()
+                break
+            else:
+                unit.tutup_pintu()
+
+############################
+
+try:
+    unit.welcome()
+    main()
+except KeyboardInterrupt:
+    unit.goodbye()
+    pass
---
+++ unit.py
@@ -0,0 +1,109 @@
+from time import sleep
+
+def print_delay(text):
+    sleep(0.5)
+    print(text)
+
+def input_delay(prompt):
+    sleep(0.5)
+    return input(prompt)
+
+def welcome():
+    print_delay('Simulasi Microwave Oven')
+    print_delay('Tekan Ctrl+C untuk menghentikan program.')
+
+def goodbye():
+    print_delay('\nSimulasi dihentikan.')
+    print_delay('Selamat tinggal!')
+
+def buka_pintu():
+    state = input_delay('Buka pintu? (n): ')
+    while state != 'y':
+        state = input_delay('Buka pintu? (n): ')
+    print_delay('Pintu terbuka.')
+
+def tutup_pintu():
+    state = input_delay('Tutup pintu? (n): ')
+    while state != 'y':
+        state = input_delay('Tutup pintu? (n): ')
+    print_delay('Pintu tertutup.')
+
+def switch_lampu(state):
+    sleep(0.5)
+    if state:
+        print_delay('Lampu menyala.')
+    else:
+        print_delay('Lampu mati.')
+
+def input_makanan():
+    return input_delay('Masukkan makanan: ')
+
+def switch_makanan(state, name):
+    sleep(0.5)
+    if name == '' or name.isspace():
+        print_delay('Microwave kosong.')
+    elif state:
+        print_delay(name + ' telah dimasukkan ke dalam microwave.')
+    else:
+        print_delay(name + ' telah dikeluarkan dari microwave.')
+
+def keluarkan_makanan(name):
+    sleep(0.5)
+    keluarkan = input_delay('Keluarkan makanan? ')
+    if keluarkan == 'y':
+        return True
+    else:
+        return False
+
+def print_mode(modes):
+    print_delay(modes)
+
+def input_mode(modes):
+    mode = input_delay('Pilih mode: ')
+    while not (mode in modes):
+        print_delay('Mode tidak valid!')
+        mode = input_delay('Pilih mode: ')
+    print_delay('Menggunakan mode ' + mode)
+    return modes.index(mode)
+
+def input_durasi():
+    durasi = input_delay('Masukkan durasi: ')
+    while not durasi.isnumeric():
+        print_delay('Durasi tidak valid!')
+        durasi = input_delay('Masukkan durasi: ')
+    print_delay('Memanaskan selama ' + durasi + ' detik')
+    return int(durasi)
+
+def switch_magnetron(state):
+    sleep(0.5)
+    if state:
+        print_delay('Magnetron menyala.')
+    else:
+        print_delay('Magnetron mati.')
+
+def switch_kipas(state):
+    sleep(0.5)
+    if state:
+        print_delay('Kipas menyala.')
+    else:
+        print_delay('Kipas mati.')
+
+def heat(mode, durasi):
+    sleep(0.5)
+    print_delay('Mulai memanaskan...')
+    print_delay('Tekan Ctrl+C untuk menghentikan pemanasan.')
+    sleep(1)
+    try:
+        for i in range(durasi, -1, -1):
+            if i > 0:
+                print_delay(str(i) + ' s')
+                sleep(1)
+        pass
+    except KeyboardInterrupt:
+        print_delay('Pemanasan dihentikan.')
+        pass
+    print_delay('Selesai memanaskan...')
+
+def beep():
+    sleep(0.5)
+    print_delay('BEEP!')
